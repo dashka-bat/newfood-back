0dc375f9dc940ce147feb91b7dc929f90ae111aa
dashka-bat <dushkab02@gmail.com>
2025-02-03 13:32:35 +0800

    commi21

--- index.ts
+++ index.ts
@@ -3,11 +3,27 @@ import { configDotenv } from "dotenv";
 import { time } from "console";
 import { food_categoryrouter } from "./router/food-category";
 import { food_router } from "./router/food";
+import { verifyToken } from "@clerk/backend/dist/tokens/verify";
 const mongoose = require("mongoose");
 const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 3004;
+const auth= async (req,res,next)=>{
+  const token=req.get("Token")
+
+  try{
+    const verified=await verifyToken(token,{
+      secretKey:process.env.CLERK_SECRET_KEY,
+    })
+    // const userId=verified.sub;
+    // req.userId=userId
+    next()
+    console.log(verified)
+  }catch{
+    res.json({status:"forbidden"})
+  }
+}
 app.use(cors());
 app.use(express.json());
 
@@ -30,8 +46,8 @@ export const connectMOngoDB = async () => {
   }
 };
 connectMOngoDB();
-app.use("/food-category", food_categoryrouter);
-app.use("/food", food_router);
+app.use("/food-category",auth, food_categoryrouter);
+app.use("/food",auth, food_router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
